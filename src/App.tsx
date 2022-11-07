f0ed3ee107e2b732b47818bfb1132a280a5e9569
MrEvgeniy1989 <mr.evgeniy1989@yandex.ru>
2022-11-07 22:57:54 +0300

    update app

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,33 @@ import React from 'react';
 import './App.css';
 import TodoList from "./TodoList";
 
+export type TasksType = {
+    id: number
+    title: string
+    isDone: boolean
+}
+
+
 function App() {
+    const todoListTitle_1 = "What to learn"
+    const todoListTitle_2 = "What to buy"
+
+    const tasks_1: Array<TasksType> = [
+        {id: 1, title: "HTML & CSS", isDone: true},
+        {id: 2, title: "JS", isDone: true},
+        {id: 3, title: "React", isDone: false},
+    ]
+
+    const tasks_2: Array<TasksType> = [
+        {id: 4, title: "Beer", isDone: false},
+        {id: 5, title: "Milk", isDone: true},
+        {id: 6, title: "Cola", isDone: true},
+    ]
+
     return (
         <div className="App">
-            <TodoList/>
+            <TodoList tasks={tasks_1} title={todoListTitle_1}/>
+            <TodoList tasks={tasks_2} title={todoListTitle_2}/>
         </div>
     );
 }
